e28f97e6133af24ebd4b298cbf4ce0fb435c0080
Joey Vinyard <jviny98@yahoo.com>
2017-08-01 16:27:35 -0400

    Removed stubbed out navbar from master branch

--- src/app/nav/nav.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'navbar',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
-})
-export class NavComponent {}
