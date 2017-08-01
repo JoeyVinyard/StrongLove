96289f61679d91cb09ec56b89da44f822b24d03a
Joey Vinyard <jviny98@yahoo.com>
2017-08-01 16:30:25 -0400

    fixed errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
   ],
   imports: [
     BrowserModule
