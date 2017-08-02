329be210a6789c299772cd3c6598a8de08308891
Joey Vinyard <jviny98@yahoo.com>
2017-08-02 09:13:43 -0400

    Routing works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component'
+import { NavComponent } from './nav/nav.component';
+import { SigninComponent } from './signin/signin.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'signin',
+        component: SigninComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
