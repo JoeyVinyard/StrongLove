b54b863a9d7a2d7c5b67e030f83e16c3ad106ad8
Joey Vinyard <jviny98@yahoo.com>
2017-08-07 14:48:05 -0400

    Async stuff done

--- src/app/publicprofile/publicprofile.component.ts
+++ src/app/publicprofile/publicprofile.component.ts
@@ -8,17 +8,17 @@ import { Profile } from '../models/profile'
   templateUrl: './publicprofile.component.html',
   styleUrls: ['./publicprofile.component.css']
 })
-export class PublicprofileComponent implements OnInit {
+export class PublicprofileComponent {
+     modelProfile = new Profile("","","","","",0,"","");
 	activeId = 1;
 
 	change(id: number){
 		console.log("changing to: ", id);
 		this.activeId = id;
 	}
-  constructor(private us: UsersService) { }
-
-  ngOnInit() {
-    console.log(this.us.getUserInfo("bobbert"))
+  constructor(private us: UsersService) {
+    this.us.getUserInfo("bobbert").then((prof) => {
+      this.modelProfile = new Profile(prof.username, prof.name, prof.gender, prof.about, prof.hobbies, prof.age, prof.langs, prof.prefgender);
+    })
   }
-
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -10,15 +10,9 @@ export class UsersService{
 		this.afd.database.ref('users/' + p.username).set(p)
 	}
 	getUserInfo(username: string){
-		console.log(username);
-		var p;
-		this.afd.database.ref('users/' + username).once('value').then((s) => {
-			var item = s.val()
-			p = new Profile(item.username, item.name, item.gender, item.about, item.hobbies, item.age, item.langs, item.prefgender);
-			console.log("yo");
-			return p;
-		})
-		console.log("hi");
+		return this.afd.database.ref('users/' + username).once('value').then(function(s) {
+			return s.val();
+		});
 	}
 	constructor(private afd: AngularFireDatabase, private router: Router){}
 }
