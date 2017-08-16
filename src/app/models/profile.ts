1afa202bf6f98375464829d2a678a42e5012a1be
Snail <adsnelling@comcast.net>
2017-08-16 13:46:26 -0400

    fixed user bullcrap

--- src/app/models/profile.ts
+++ src/app/models/profile.ts
@@ -1,15 +1,14 @@
 export class Profile{
 	constructor(public uid: string,
-					public username: string,
-					public name: string,
-					public gender: string,
-					public imglink: string,
-					public about: string,
-					public city: string,
-					public hobbies: string[],
-					public age: number,
-					public langs: string[],
-					public prefgender: string,
-					public contact: string){}
-
+				public username: string,
+				public name: string,
+				public gender: string,
+				public imglink: string,
+				public about: string,
+				public city: string,
+				public hobbies: string[],
+				public age: number,
+				public langs: string[],
+				public prefgender: string,
+				public contact: string){}
 }
